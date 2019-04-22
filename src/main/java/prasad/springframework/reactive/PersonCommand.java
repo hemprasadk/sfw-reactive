116ca02b387be0d3573d1958026e55d8b5a0fcec
hekenderi <hemprasadk@gmail.com>
2019-04-21 20:32:51 -0400

    intial upload

---
+++ src/main/java/prasad/springframework/reactive/PersonCommand.java
@@ -0,0 +1,24 @@
+package prasad.springframework.reactive;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonCommand {
+
+    public PersonCommand (Person person)
+    {
+        this.firstName = person.getFirstName();
+        this.lastName = person.getLastName();
+    }
+
+    private  String firstName;
+    private  String lastName;
+
+    public String sayMyName(){
+        return "My Name is " + firstName + " " + lastName + ".";
+    }
+}
